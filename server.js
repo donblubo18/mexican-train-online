2f8c07c6ccd917473f010c16271b907054528c8d
donblubo18 <donblubo18@gmail.com>
2026-06-15 21:33:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+
+// OPLOSSING CORS BUG SERVER: Accepteer expliciet alle polling en websocket verbindingen
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+    },
+    allowEIO3: true
+});
 
 app.use(express.static('public'));
 
