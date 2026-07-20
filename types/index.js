32807d9fc839b10b134eff0fbf526c3b7ec64ee2
donblubo18 <donblubo18@gmail.com>
2026-07-20 16:54:44 +0200

    Delete types directory

--- types/index.js
+++
@@ -1,6 +0,0 @@
-// Blueprints om datastructuren waterdicht te synchroniseren tussen server en client
-module.exports = {
-    createRequiredDouble: () => ({ active: false, value: null, targetId: null }),
-    createPlayerState: (id, name) => ({ id, name, isOpen: false, train: [], totalScore: 0 }),
-    createSpectatorState: (id, name) => ({ id, name })
-};
