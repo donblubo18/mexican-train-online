d5745e59b0a0dd962c08f804ba205670caeb8431
donblubo18 <donblubo18@gmail.com>
2026-07-20 17:32:57 +0200

    Update audio-controller.ts

--- public/engine/audio-controller.ts
+++ public/engine/audio-controller.ts
@@ -9,24 +9,20 @@ export class AudioController {
         };
     }
 
-    // Moet aangeroepen worden bij de eerste klik (Join, Spectate of Start)
     public unlock(): void {
-        console.log("AudioController: Browser toestemming aanvragen...");
         Object.values(this.sounds).forEach(audio => {
             audio.muted = true;
             audio.play().then(() => {
                 audio.pause();
                 audio.currentTime = 0;
                 audio.muted = false;
-            }).catch(err => console.warn("Audio unlock mislukt:", err));
+            }).catch(() => {});
         });
     }
 
     public play(type: 'turn' | 'trainOpen' | 'knock'): void {
         if (this.sounds[type]) {
-            this.sounds[type].play().catch(e => 
-                console.log("Audio geblokkeerd door browser, wacht op interactie.", e)
-            );
+            this.sounds[type].play().catch(() => {});
         }
     }
 }
