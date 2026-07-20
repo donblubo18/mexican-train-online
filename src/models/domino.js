9f1c3a76d8b38d9504d0a56f8c111a0b2ebede56
donblubo18 <donblubo18@gmail.com>
2026-07-20 17:30:00 +0200

    Update domino.js

--- src/models/domino.js
+++ src/models/domino.js
@@ -1,19 +1,11 @@
-class Domino {
-    // Controleert of een steen aansluit op het openstaande cijfer (tail) van een trein
-    static canConnect(stone, tail) {
-        if (!Array.isArray(stone) || stone.length !== 2) return false;
-        return stone[0] === tail || stone[1] === tail;
+function generateDeck(max) {
+    let deck = [];
+    for (let i = 0; i <= max; i++) {
+        for (let j = i; j <= max; j++) {
+            deck.push([i, j]);
+        }
     }
-
-    // Draait de steen om indien de rechterkant moet aansluiten
-    static orient(stone, tail) {
-        if (stone[0] === tail) return [stone[0], stone[1]];
-        if (stone[1] === tail) return [stone[1], stone[0]];
-        return null;
-    }
-
-    static isDouble(stone) {
-        return Array.isArray(stone) && stone[0] === stone[1];
-    }
+    return deck.sort(() => Math.random() - 0.5);
 }
-module.exports = Domino;
+
+module.exports = { generateDeck };
